6bc562f80a19664db9466aad20a61edb192fd018
felipemimoura <felipemimoura@gmail.com>
2023-02-03 16:24:26 -0300

    feat: add button component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello Timer</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="sucess" />
     </div>
   );
 }
