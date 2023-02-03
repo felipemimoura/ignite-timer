b519378d26a71b842eb9cea6eb933e7c760876b7
felipemimoura <felipemimoura@gmail.com>
2023-02-03 17:21:30 -0300

    feat: use global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { GlobaStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme} >
+      <GlobaStyle /> 
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
