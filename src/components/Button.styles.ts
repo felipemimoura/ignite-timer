a9e9039855e79e2dd416352ef12c66bf06ec89da
felipemimoura <felipemimoura@gmail.com>
2023-02-03 16:24:06 -0300

    feat: build a button to use styled components to customize

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,23 @@
+import styled, {css} from "styled-components";
+
+type ButtonVariant = "primary" | "secondary" | "danger" | "sucess";
+
+interface ButtonConteinerProps {
+  variant: ButtonVariant;
+}
+
+const backgroundColorsVariant = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  sucess: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonConteinerProps>`
+  width: 100px;
+  height: 40px;
+  
+  ${props => {
+    return css`background-color: ${backgroundColorsVariant[props.variant]}`
+  }}
+`;
