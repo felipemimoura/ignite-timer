d4590cc9b47f2d00174b20e9bf26e4e302e883d2
felipemimoura <felipemimoura@gmail.com>
2023-02-04 15:50:06 -0300

    fix: change font size to 1rem

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -21,7 +21,7 @@ export const GlobaStyle = createGlobalStyle`
     body, input, textarea, button {
         font-family: "Roboto", sans-serif;
         font-weight: 400;
-        font-size: 62.5%;
+        font-size: 1rem;
     }
 
 `
