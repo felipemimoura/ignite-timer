dfc19df56c5cce13a5c8c38dc68a18d486f60670
felipemimoura <felipemimoura@gmail.com>
2023-02-04 14:44:33 -0300

    feat: create default layout to project

---
+++ src/pages/layouts/DefaultLayout.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../../components/Header'
+import { Outlet } from 'react-router-dom'
+export const DefaultLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  )
+}
