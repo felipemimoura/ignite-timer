957e59c33ff201f10215accf881732364404924f
felipemimoura <felipemimoura@gmail.com>
2023-02-04 14:41:03 -0300

    refactor: remove component not necessary

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer } from './Button.styles'
-
-interface ButtonProps {
-  variant?: 'primary' | 'secondary' | 'danger' | 'sucess'
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
